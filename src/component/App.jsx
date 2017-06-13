540b3b79182faf8b3ce1ab2fc7d4f3a0777c623f
Anthony <anthonyguitarman13@gmail.com>
2017-06-12 20:04:33 -0600

    revise

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -18,7 +18,7 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="title">
-                Reminder Pro 
+                Reminder Pro!
                 </div>
                 <div className="form-inline">
                     <div className="form-group">
